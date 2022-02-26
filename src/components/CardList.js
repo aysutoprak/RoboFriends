15bdeb858e6dd74ec237d1b0a7fd702003f6d34d
aysutoprak <moonwater99@hotmail.com>
2022-02-26 14:59:10 +0300

    commit

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,18 +1,21 @@
 import React from "react";
 import Cards from "./Cards";
 
-function CardList({ robots }) {
-  const cardsArray = robots.map((user, i) => {
-    return (
-      <Cards
-        key={i}
-        id={robots[i].id}
-        name={robots[i].name}
-        email={robots[i].email}
-      />
-    );
-  });
-  return <div>{cardsArray}</div>;
-}
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {robots.map((user, i) => {
+        return (
+          <Cards
+            key={i}
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default CardList;
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import SearchBox from "../components/SearchBox";
 import CardList from "../components/CardList";
 import "./App.css";
 import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
 
 class App extends React.Component {
   constructor() {
@@ -37,7 +38,9 @@ class App extends React.Component {
         <h1 className="f2">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
         </Scroll>
       </div>
     );
